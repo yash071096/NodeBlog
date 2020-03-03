413503a8a07bb307fe857ba6445cb7959298de9e
yash <yash071096@gmail.com>
2020-03-03 14:34:45 -0600

    First

---
+++ routes/categories.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+
+router.get('/show/:category', function(req, res, next) {
+  var posts = db.get('posts');
+  posts.find({category: req.params.category},{},function(err, posts){
+    res.render('index',{
+        'title': req.params.category,
+        'posts': posts
+       });
+  });
+   
+});
+
+
+router.get('/add', function(req, res, next) {
+	
+		res.render('addcategory',{
+  	    'title': 'Add category',
+  	    
+       });
+});
+router.post('/add', function(req, res, next) {
+  // Get Form Values
+  var name = req.body.name;
+  
+
+
+  //form validation
+  req.checkBody('name', 'Name field is required').notEmpty();
+  
+  //CHeck errors
+  var errors = req.validationErrors();
+  if(errors){
+  	res.render('addpost' ,{
+  		"errors": errors,
+
+  	});
+  } else{
+  	var categories = db.get('categories');
+  	categories.insert({
+  		"name": name,
+  		
+  	},function(err, post){
+  		if(err){
+  			res.send(err);
+  		} else{
+  			req.flash('success', 'Category Added');
+  			res.location('/');
+  			res.redirect('/');
+  		}
+  	});
+  }
+});
+
+module.exports = router;
